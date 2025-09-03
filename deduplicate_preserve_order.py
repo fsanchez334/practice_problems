58623a752f2ae0e5977d4463265ec2dc2574df3a
Fernando <fernandosanchez955@gmail.com>
2025-09-02 22:34:52 -0400

    Removed unnecessary elif statement and moved assert test cases

--- deduplicate_preserve_order.py
+++ deduplicate_preserve_order.py
@@ -18,9 +18,6 @@ Pseudocode
 def deduplicate_keep_order(xs: list[int]) -> list[int]:
     if len(xs) <= 1:
         return xs
-    elif len(set(xs)) == 1:
-        unique_value = [xs[0]]
-        return unique_value
     else:
         tracker_set = set()
         final_result = []
@@ -29,14 +26,4 @@ def deduplicate_keep_order(xs: list[int]) -> list[int]:
                 final_result.append(number)
                 tracker_set.add(number)
         return final_result
-    
-assert deduplicate_keep_order([3, 1, 3, 2, 1]) == [3, 1, 2]
-assert deduplicate_keep_order([]) == []
-assert deduplicate_keep_order([7]) == [7]
-assert deduplicate_keep_order([5, 5, 5, 5]) == [5]
-assert deduplicate_keep_order([1, 2, 3, 4]) == [1, 2, 3, 4]
-assert deduplicate_keep_order([1, 2, 1, 3, 2, 4]) == [1, 2, 3, 4]
-assert deduplicate_keep_order([1000, 1000, 2000, 3000, 2000]) == [1000, 2000, 3000]
-assert deduplicate_keep_order([-1, -1, -2, -3, -2]) == [-1, -2, -3]
-assert deduplicate_keep_order([2, 1, 2, 3, 1, 4]) == [2, 1, 3, 4]
-assert deduplicate_keep_order(list(range(1000)) + list(range(500))) == list(range(1000))
+
